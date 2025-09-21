fe64c80b044e5d61e53ff6ea23e028541bcb0a68
ErvinLima <ervinlima41@gmail.com>
2025-09-21 13:44:56 -0300

    atualizando

--- app/exercícios/desafio09.py
+++ app/exercícios/desafio09.py
@@ -1,5 +1,13 @@
 n1 = int(input('de qual numero deseja saber a taboada: '))
 
-t = n1 *0, n1 *1, n1 *2, n1*3, n1*4, n1*5, n1*6, n1*7, n1*8, n1*9, n1*10
-
-print('segue a taboada do numero {} \n {}'.format(n1, t))
+print('{} x {} = {}'.format(n1, 1, n1*0))
+print('{} x {} = {}'.format(n1, 1, n1*1))
+print('{} x {} = {}'.format(n1, 1, n1*2))
+print('{} x {} = {}'.format(n1, 1, n1*3))
+print('{} x {} = {}'.format(n1, 1, n1*4))
+print('{} x {} = {}'.format(n1, 1, n1*5))
+print('{} x {} = {}'.format(n1, 1, n1*6))
+print('{} x {} = {}'.format(n1, 1, n1*7))
+print('{} x {} = {}'.format(n1, 1, n1*8))
+print('{} x {} = {}'.format(n1, 1, n1*9))
+print('{} x {} = {}'.format(n1, 1, n1*10))
